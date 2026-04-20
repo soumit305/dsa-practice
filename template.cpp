19ace830ff22e7f447e031d13cf95e903a4d2751
Soumit Mishra <soumitsm2000@gmail.com>
2026-04-20 05:51:09 +0530

    Setup: template + first test program

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Problem: [Name and link]
+// Time: O(?)
+// Space: O(?)
+
+class Solution {
+public:
+    // function signature here
+};
+
+int main() {
+    // test cases
+    return 0;
+}
