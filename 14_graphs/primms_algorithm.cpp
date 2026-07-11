383b927c1217ee480e47a8f03f3839e909a45c5e
Soumit Mishra <soumitsm2000@gmail.com>
2026-07-11 07:42:21 +0530

    Primm Algorithm

---
+++ 14_graphs/primms_algorithm.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    int spanningTree(int V, vector<vector<int>>& edges) {
+        vector<vector<pair<int, int>>> adj(V);
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int wt = edges[i][2];
+            adj[u].push_back({v, wt});
+            adj[v].push_back({u, wt});
+        }
+
+        // Priority Queue -> {wt,node,parent}
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>>
+            pq;
+        vector<pair<int, int>> mstEdges;
+        vector<int> visited(V, 0);
+        int sum = 0;
+        pq.push({0, {0, -1}});
+
+        // Primm's Algorithm
+        while (!pq.empty()) {
+            auto it = pq.top();
+            pq.pop();
+            int weight = it.first;
+            int node = it.second.first;
+            int parent = it.second.second;
+            if (!visited[node]) {
+                visited[node] = 1;
+                mstEdges.push_back({node, parent});
+                sum += weight;
+            }
+            for (auto adjacent : adj[node]) {
+                int nnode = adjacent.first;
+                int nweight = adjacent.second;
+                if (!visited[nnode]) {
+                    pq.push({nweight, {nnode, node}});
+                }
+            }
+        }
+
+        return sum;
+    }
+};
