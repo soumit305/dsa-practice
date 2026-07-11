38d69263bedb93845e78c51b73561bf4dcccdc64
Soumit Mishra <soumitsm2000@gmail.com>
2026-07-11 09:16:18 +0530

    Disjoint Set

--- 14_graphs/disjoint_set.cpp
+++ 14_graphs/disjoint_set.cpp
@@ -1,16 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#include <bits/stdc++.h>
-using namespace std;
-
 class DisjointSet {
-    vector<int> rank, parent;
+    vector<int> rank, parent, size;
 
    public:
     DisjointSet(int n) {
         rank.resize(n + 1, 0);
         parent.resize(n + 1);
+        size.resize(n + 1, 1);
         for (int i = 0; i < n + 1; i++) {
             parent[i] = i;
         }
@@ -36,22 +34,36 @@ class DisjointSet {
             rank[ultp_u]++;
         }
     }
+
+    void unionBySize(int u, int v) {
+        int ultp_u = findUPar(u);
+        int ultp_v = findUPar(v);
+        if (ultp_u == ultp_v)
+            return;
+        if (size[ultp_u] < size[ultp_v]) {
+            parent[ultp_u] = ultp_v;
+            size[ultp_v] += size[ultp_u];
+        } else {
+            parent[ultp_v] = ultp_u;
+            size[ultp_u] += size[ultp_v];
+        }
+    }
 };
 
 int main() {
     DisjointSet ds(7);
-    ds.unionByRank(1, 2);
-    ds.unionByRank(2, 3);
-    ds.unionByRank(4, 5);
-    ds.unionByRank(6, 7);
-    ds.unionByRank(5, 6);
+    ds.unionBySize(1, 2);
+    ds.unionBySize(2, 3);
+    ds.unionBySize(4, 5);
+    ds.unionBySize(6, 7);
+    ds.unionBySize(5, 6);
 
     if (ds.findUPar(3) == ds.findUPar(7))
         cout << "Same" << endl;
     else
         cout << "Not Same" << endl;
 
-    ds.unionByRank(3, 7);
+    ds.unionBySize(3, 7);
 
     if (ds.findUPar(3) == ds.findUPar(7))
         cout << "Same" << endl;
